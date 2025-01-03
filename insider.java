cc06f3c8ed97cc70622023469603d1af8ffed1ec
mrnajych <mrnajych@gmail.com>
2025-01-03 16:47:43 +0530

    we can add

--- insider.java
+++ insider.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class sample{
     public static void main(String args[]){
+        int a=5, b=6;
         System.out.println("Thank You");
+        System.out.print(a+b);
     }
 }
